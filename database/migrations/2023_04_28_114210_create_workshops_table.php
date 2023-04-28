37799ffab4b7d3c8da1e3636dea11473305c8422
Joey Peschier <62258312+joeyyeoj@users.noreply.github.com>
2023-04-28 13:58:49 +0200

    Base migrations

--- database/migrations/2023_04_28_114210_create_workshops_table.php
+++ database/migrations/2023_04_28_114210_create_workshops_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('workshops', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->date('date_of_birth');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('subject');
+            $table->string('content_path');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('workshops');
     }
 };
