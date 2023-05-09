420a384e42db6aa83623dc0c4776437f625624fc
Peter <petervanrijn@hotmail.nl>
2023-05-09 10:57:26 +0200

    Revert "make factory fucntion for user en workshop"
    This reverts commit fc42ad4d1b6fedff74e9c657a08d0ab5ea3c5b9a.

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'firstname',
-        'lastname'.
+        'name',
         'email',
+        'password',
     ];
 
     /**
