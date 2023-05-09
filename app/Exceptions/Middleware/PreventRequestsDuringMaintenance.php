e7e0c32160693495077df86ae4eb76260544991e
Joey Peschier <62258312+joeyyeoj@users.noreply.github.com>
2023-05-09 16:24:36 +0200

    More stuff

--- app/Exceptions/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Exceptions/Middleware/PreventRequestsDuringMaintenance.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Exceptions\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
 
--- app/Exceptions/Middleware/TrimStrings.php
+++ app/Exceptions/Middleware/TrimStrings.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Exceptions\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
 
