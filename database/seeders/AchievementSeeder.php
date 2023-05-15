27a7c5a4c7b5c4d7b757cac8b2b6e16d3c3d701a
daniellooy <daniel.looijestijn@hotmail.com>
2023-05-15 19:30:30 +0200

    migrations en seeders van achievements zijn klaar

--- database/seeders/AchievementSeeder.php
+++ database/seeders/AchievementSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
 
 class AchievementSeeder extends Seeder
 {
@@ -12,6 +13,21 @@ class AchievementSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('achievements')->insert([
+            'name' => "LOB-influencer",
+            'description' => "Hoeveel kids heb je in een klas lesgegeven?",
+        ]);
+        DB::table('achievements')->insert([
+            'name' => "Carriereswitch?",
+            'description' => "Hoe hoog worden je workshops beoordeeld?",
+        ]);
+        DB::table('achievements')->insert([
+            'name' => "GLobetrotter",
+            'description' => "Ga mee naar projecten",
+        ]);
+        DB::table('achievements')->insert([
+            'name' => "Ga er maar uit!",
+            'description' => "Stuur een kind naar waar hij vandaan komt",
+        ]);
     }
 }
